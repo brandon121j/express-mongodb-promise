e466e113c3b340d5d938c1eb0f760e9f0a4db6f2
Brandon <brandon121j@gmail.com>
2021-09-27 12:03:43 -0400

    Addition of getAllTodo Route

--- routes/todo/todoRouter.js
+++ routes/todo/todoRouter.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const { getAllTodo } = require('./controller/todoController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.send('todo');
+
+    getAllTodo({})
+        .then(payload => {
+            res.json({ message: "SUCCESS", payload })
+        })
+        .catch(error => {
+            res.status(500).json({ message: "FAILURE", error: error.message })
+        });
 });
 
 module.exports = router;
