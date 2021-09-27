3becab4ade5893211c18cee1dc268f12a2b24f03
Brandon <brandon121j@gmail.com>
2021-09-27 15:41:11 -0400

    Addition of getAllRecipes request

--- routes/recipe/controller/recipeController.js
+++ routes/recipe/controller/recipeController.js
@@ -1,8 +1,20 @@
 const Recipe = require('../model/Recipe');
 
-function getAllRecipes(body)
+function getAllRecipes(body) {
+    return new Promise((resolve, reject) => {
+
+        Recipe
+            .find(body)
+            .then(payload => {
+                resolve(payload)
+            })
+            .reject(error => {
+                reject(error)
+            });
+    });
+}
 
 
 module.exports = {
-
+    getAllRecipes,
 }
